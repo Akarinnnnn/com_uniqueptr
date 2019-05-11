648f813dbf862e40f5dc384d147ad91cf048c006
Akarinnnnn <1207247008@qq.com>
2019-05-11 09:50:37 +0800

    uinque_com

---
+++ com_uniqueptr.h
@@ -0,0 +1,16 @@
+﻿#include <memory>
+#include <combaseapi.h>
+namespace COM_helper
+{
+	class COM_Deleter
+	{
+	public:
+		inline void operator() (IUnknown* target)
+		{
+			if (target != nullptr)
+				target->Release();
+		}
+	};
+	template <typename T>
+	using unique_com = std::unique_ptr<T, COM_Deleter>;
+}
